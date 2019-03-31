f017464ca3c57c89458128eedc0e0b1fad8cca9f
itsmeprakhar22 <itsmeprakhar22@gmail.com>
2019-03-31 19:32:45 +0530

    Encrypt and Decrypt a message

--- app/src/main/java/com/wordpress/uniquecoder/encryptednotes/MainActivity.java
+++ app/src/main/java/com/wordpress/uniquecoder/encryptednotes/MainActivity.java
@@ -2,12 +2,114 @@ package com.wordpress.uniquecoder.encryptednotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStoreException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText toenc, todec;
+    TextView enc, dec;
+    Button encrypt, decrypt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String pwd="SHARAN";
+        setUI();
+
+        encrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String e = encrypt(toenc.getText().toString(), pwd);
+                    enc.setText(e);
+                    Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        decrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                String d=decrypt(todec.getText().toString(),pwd);
+                dec.setText(d);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
+
+    private String decrypt(String data, String pwd) throws Exception{
+        SecretKeySpec key=generatekey(pwd);
+        Cipher c=Cipher.getInstance("AES");
+        c.init(Cipher.DECRYPT_MODE,key);
+        byte[] decodedVal=Base64.decode(data,Base64.DEFAULT);
+        byte[] decVal=c.doFinal(decodedVal);
+        String decryptedVal=new String(decVal);
+        return decryptedVal;
+
+    }
+
+    public void setUI() {
+        toenc = findViewById(R.id.toEnc);
+        todec = findViewById(R.id.toDec);
+        encrypt = findViewById(R.id.encrypt);
+        decrypt = findViewById(R.id.decrypt);
+        enc = findViewById(R.id.enc);
+        dec = findViewById(R.id.dec);
+
+    }
+
+    public String encrypt(String data,String pwd) throws Exception{
+        SecretKeySpec key=generatekey(pwd);
+        Cipher c=Cipher.getInstance("AES");
+        c.init(Cipher.ENCRYPT_MODE,key);
+        byte[] encVal=c.doFinal(data.getBytes());
+        String encryptedVal=Base64.encodeToString(encVal,Base64.DEFAULT);
+        return encryptedVal;
+    }
+
+    private SecretKeySpec generatekey(String pwd) throws Exception {
+        final MessageDigest digest=MessageDigest.getInstance("SHA-256");
+        byte[] bytes=pwd.getBytes("UTF-8");
+        digest.update(bytes,0,bytes.length);
+        byte[] key=digest.digest();
+        SecretKeySpec secretKeySpec=new SecretKeySpec(key,"AES");
+        return secretKeySpec;
+    }
+
+
 }
